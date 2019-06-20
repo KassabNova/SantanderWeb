febbf69f399aa440fa405dd47b0ccf5fa1601f05
Carlos Kassab <ckassabandre@gmail.com>
2019-06-19 22:16:31 -0500

    Localstorage Session
    Added NuGet package and started local storage session development

--- SantanderBlazor/SantanderBlazor.Client/Startup.cs
+++ SantanderBlazor/SantanderBlazor.Client/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Http;
 namespace SantanderBlazor.Client
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<AppState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
